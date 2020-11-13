7d50103916f3cbda5ec0b5624a01817be4e4a4d4
Sanjoy Kumar Saha <sanjoyksaha0@gmail.com>
2020-11-14 02:01:41 +0600

    Fixed authenticated user can see their reports and Update readme file with details.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <a href="{{ route('monthlyPaymentReport') }}" class="nav-link">Monthly Payment Report</a>
+                        @auth
+                            <a href="{{ route('monthlyPaymentReport') }}" class="nav-link">Monthly Payment Report</a>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
